f063a02e38be6b96b2aa0856a6f0f452ab2c29d2
jessichaalvina <146162362+jessichaalvina@users.noreply.github.com>
2024-02-06 16:13:53 +0700

    Create Main.java

---
+++ perulangan/4_tipeData/src/pkg4_tipedata/Main.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg4_tipedata;
+
+import javax.swing.text.html.HTML;
+import static javax.swing.text.html.HTML.Tag.S;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        /*
+        Tipe Data
+        1. Angka
+        integer        -> int   -> bilangan bulat
+        floating point -> float -> bilangan berkoma terbatas
+        double         -> double -> bilangan berkoma
+        2. text
+        string         -> kata (saya)
+        char           -> Karakter (A) (B) (=)
+        3. boolean
+        boolean        -> true/false
+         */
+        // tipe data nama_variabel = isi_variabel;
+        
+    int umur =13 ;
+    System.out.println("umur = " + umur + " tahun");
+    
+    float berat = (float) 79.9;
+    System.out.println("berat = " + berat + " kg");
+    
+    double panjang = 12.8999999 ;
+    System.out.println("panjang = " + panjang + " m");
+    
+    String nama = "Syifa Hadju" ;
+    System.out.println ("Nama = " + nama );
+    
+    char gender = 'L';
+    System.out.println("jenis kelamin = " + gender );
+    
+    boolean sehat = true;
+    System.out.println("Sehat= " + sehat);
+    
+    
+    
+    }
+    
+}
